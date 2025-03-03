f5a9c8c1df87b80db4454976df2474b8ea0a00ae
MauroG07 <71202727+MauroG07@users.noreply.github.com>
2025-03-03 20:31:21 +0000

    Models

--- src/api/models.py
+++ src/api/models.py
@@ -6,6 +6,7 @@ db = SQLAlchemy()
 
 
 class Users(db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
@@ -14,7 +15,7 @@ class Users(db.Model):
     last_name = db.Column(db.String(), unique=False, nullable=True)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<User: {self.email}>'
 
     def serialize(self):
         # Do not serialize the password, its a security breach
@@ -27,18 +28,30 @@ class Users(db.Model):
 
 class Products(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True, nullable=False)
-    description = db.Column(db.String, unique=False, nullable=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    description = db.Column(db.String(120), unique=False, nullable=True)
     price = db.Column(db.Float, unique=False, nullable=False)
 
+    def __repr__(self):
+        return f'<Product: {self.name}>'
+    
+    def serialize(self):
+        return{'id': self.id,
+               'name': self.name,
+               'description': self.description,
+               'price': self.price}
+    
 
 class Bills(db.Model):
+    __tablename__ = "bills"
     id = db.Column(db.Integer, primary_key=True)
-    create_at = db.Column(db.DateTime, unique=False, nullable=False, default=datetime)  # Default, el dia de creación
+    create_at = db.Column(db.DateTime, unique=False, nullable=False, default=datetime.utcnow())  # Default, el dia de creación
     total = db.Column(db.Float, unique=False, nullable=False)
     bill_address = db.Column(db.String, unique=False, nullable=True)
     status = db.Column(db.Enum("pending", "payed", "canceled", name="status"), unique=False, nullable=False)
     payment = db.Column(db.Enum("visa", "amex", "paypal", name="payment"), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))  # Columna que guarda el Indice
+    user_to = db.relationship("Users", foreign_keys=[user_id], backref=db.backref("bills_to", lazy="select"))  # Define la relación con la Clase y la Columna en cuestión. El backref crea automaticamente un atributo ("bills to") en el modelo/tabla relacionado (Users), (va a crear un indice (listado) con todas las facturas) 
 
 
 class BillItems(db.Model):
@@ -47,6 +60,10 @@ class BillItems(db.Model):
     price_per_unit = db.Column(db.Float, unique=False, nullable=False)
     quantity = db.Column(db.Integer, unique=False, nullable=False)
     total_price = db.Column(db.Float, unique=False, nullable=False)
+    bill_id = db.Column(db.Integer, db.ForeignKey("bills.id"))
+    bill_to = db.relationship("Bills", foreign_keys=[bill_id], backref=db.backref("bill_items", lazy="select"))
+    product_id = db.Column(db.Integer, db.ForeignKey("products.id"))
+    product_to = db.relationship("Products", foreign_keys=[product_id], backref=db.backref("bill_items", lazy="select"))
 
 
 class Followers(db.Model):
@@ -58,33 +75,39 @@ class Followers(db.Model):
 
 class Posts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, unique=False, nullable=True)
+    title = db.Column(db.String(120), unique=False, nullable=True)
     description = db.Column(db.String, unique=False, nullable=True)
-    body = db.Column(db.String, unique=False, nullable=True)
-    date = db.Column(db.DateTime, unique=False, nullable=False, default=datetime)
-    image_url = db.Column(db.String, unique=False, nullable=True)
+    body = db.Column(db.String(120), unique=False, nullable=True)
+    date = db.Column(db.DateTime, unique=False, nullable=False, default=datetime.utcnow())
+    image_url = db.Column(db.String(120), unique=False, nullable=True)
     user_id = db.Column(db.Integer, unique=True, nullable=False)
 
 
 class Comments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String, unique=False, nullable=True)
+    body = db.Column(db.String(120), unique=False, nullable=True)
     user_id = db.Column(db.Integer, unique=True, nullable=False)
     post_id = db.Column(db.Integer, unique=True, nullable=False)
 
 
 class Medias(db.Model):
-    id = db.column(db.Integer, primary_key=True)
-    type = db.column(db.Enum("enum1", "enum2", "enum3", name="type"), unique=False, nullable=False)
-    url = db.Column(db.String, unique=False, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.Enum("enum1", "enum2", "enum3", name="type"), unique=False, nullable=False)
+    url = db.Column(db.String(120), unique=False, nullable=False)
     post_id = db.Column(db.Integer, unique=True, nullable=False)
 
 
-class CharacterFavourites(db.Model):
-    __tablename__ = "character_favourites"  # Esto es necesario para que Flask interprete de esta manera el nombre y no lo haga de otra forma distinta
-    id = db.column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, unique=True, nullable=False)
-    character_id = db.Column(db.Integer, unique=True, nullable=False)
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=True)
+    diameter = db.Column(db.String(120), unique=False, nullable=True)
+    rotation_period = db.Column(db.String(120), unique=False, nullable=True)
+    orbital_period = db.Column(db.String(120), unique=False, nullable=True)
+    gravity = db.Column(db.String(120), unique=False, nullable=True)
+    population = db.Column(db.String(120), unique=False, nullable=True)
+    climate = db.Column(db.String(120), unique=False, nullable=True)
+    terrain = db.Column(db.String(120), unique=False, nullable=True)
+
 
 class PlanetFavourite(db.Model):
     __tablename__ = "planet_favourites"  # Esto es necesario para que Flask interprete de esta manera el nombre y no lo haga de otra forma distinta
@@ -92,25 +115,21 @@ class PlanetFavourite(db.Model):
     user_id = db.Column(db.Integer, unique=True, nullable=False)
     planet_id = db.Column(db.Integer, unique=True, nullable=False)
 
-class Planets(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=False, nullable=True)
-    diameter = db.Column(db.String, unique=False, nullable=True)
-    rotation_period = db.Column(db.String, unique=False, nullable=True)
-    orbital_period = db.Column(db.String, unique=False, nullable=True)
-    gravity = db.Column(db.String, unique=False, nullable=True)
-    population = db.Column(db.String, unique=False, nullable=True)
-    climate = db.Column(db.String, unique=False, nullable=True)
-    terrain = db.Column(db.String, unique=False, nullable=True)
 
 class Characters(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=False, nullable=True)
-    height = db.Column(db.String, unique=False, nullable=True)
-    mass = db.Column(db.String, unique=False, nullable=True)
-    hair_color = db.Column(db.String, unique=False, nullable=True)
-    skin_color = db.Column(db.String, unique=False, nullable=True)
-    eye_color = db.Column(db.String, unique=False, nullable=True)
-    birth_year = db.Column(db.String, unique=False, nullable=True)
-    gender = db.Column(db.String, unique=False, nullable=False)
+    name = db.Column(db.String(120), unique=False, nullable=True)
+    height = db.Column(db.String(120), unique=False, nullable=True)
+    mass = db.Column(db.String(120), unique=False, nullable=True)
+    hair_color = db.Column(db.String(120), unique=False, nullable=True)
+    skin_color = db.Column(db.String(120), unique=False, nullable=True)
+    eye_color = db.Column(db.String(120), unique=False, nullable=True)
+    birth_year = db.Column(db.String(120), unique=False, nullable=True)
+    gender = db.Column(db.String(120), unique=False, nullable=False)
 
+
+class CharacterFavourites(db.Model):
+    __tablename__ = "character_favourites"  # Esto es necesario para que Flask interprete de esta manera el nombre y no lo haga de otra forma distinta
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, unique=True, nullable=False)
+    character_id = db.Column(db.Integer, unique=True, nullable=False)
--- src/api/routes.py
+++ src/api/routes.py
@@ -4,7 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-from api.models import db, Users
+from api.models import db, Users, Products
 
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,78 @@ def handle_hello():
     response_body = {}
     response_body["message"] = "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     return (response_body), 200
+
+@api.route('/users', methods=['GET'])
+def users():
+    response_body = { }
+    rows = db.session.execute(db.select(Users)).scalars()  #Scalars devuelve una lista
+    # Opcion 1 : Standard
+    """ results = []
+    for row in rows:
+        results.append(row.serialize()) """
+    # Opcion 2 : Comprension de listas - Se usa más que la opción 1
+    # Variable = [ target for indivivdual en iterables ]
+    results = [ row.serialize() for row in rows ]
+    response_body["message"] = f'Listado de Usuarios'
+    response_body["results"] = results
+    return (response_body), 200
+
+
+@api.route('/products', methods=['GET', 'POST'])
+def products():
+    response_body = { }
+    if request.method == 'GET':
+        rows = db.session.execute(db.select(Products)).scalars()
+        results = [row.serialize() for row in rows]
+        response_body['results'] = results
+        response_body['message'] = f'Respuesta pata el motodo {request.method}'
+        return (response_body), 200
+    if request.method == 'POST':
+        data = request.json
+        print(data, type(data))
+        row = Products(name=data['name'],
+                       description=data.get('description', "n/a"),
+                       price=data['price'])
+        db.session.add(row)
+        db.session.commit()
+        response_body['message'] = f'Respuesta pata el motodo {request.method}'
+        response_body['results'] = row.serialize()
+        return (response_body), 200
+
+
+
+@api.route('/products/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def product(id):
+    response_body = { }
+    row = db.session.execute(db.select(Products).where(Products.id == id)).scalar()
+    if not row:
+        response_body['message'] = f'El producto id {id} no existe'
+        return response_body, 404
+    
+    # Validar que el usuario pueda ver, modificar o borrar el producto
+    if request.method == 'GET':
+        response_body['result'] = row.serialize()
+        response_body['message'] = f'Respuesta pata el motodo {request.method} del id: {id}'
+        return (response_body), 200
+    if request.method == 'PUT':
+        data = request.json
+        row.name = data.get('name')
+        # Aca solo modifico los datos que necesito actualizar, manteniendo el resto
+        
+        # Opción 1
+            # foo = data['description'] if data.get('description') else None
+        
+        #Opción 2
+        row.description = data.get('description', row.description) 
+        row.price = data['price']
+        db.session.commit()
+        response_body['message'] = f'Respuesta pata el motodo {request.method} del id: {id}'
+        response_body['result'] = row.serialize()
+        return (response_body), 200
+    if request.method == 'DELETE':
+        # La pregunta es: Borro o deshabilito ?
+        db.session.delete(row)
+        db.session.commit()
+        response_body['message'] = f'Se eliminó {request.method} del id: {id}'
+        response_body['results'] = {}
+        return (response_body), 200
